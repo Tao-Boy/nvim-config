c325eb1e32be098c783179a2821c1e349b00d2ad
root <root@localhost.localdomain>
2025-11-29 20:45:11 +0800

    11-29

--- lua/plugins/tools/markdown-preview.lua
+++ lua/plugins/tools/markdown-preview.lua
@@ -1,9 +1,11 @@
 return {
-	"brianhuster/live-preview.nvim",
-	dependencies = {
-		"folke/snacks.nvim",
-	},
-	opts = {
-		port = 8080,
-	},
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && npm install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+		vim.g.mkdp_port = "8080"
+    vim.g.mkdp_echo_preview_url = 1
+	end,
+	ft = { "markdown" },
 }
